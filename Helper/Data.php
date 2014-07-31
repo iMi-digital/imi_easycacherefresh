5a6e1fbf023381be405f2c871b68c2df50ce1723
akn <a.kellmann@imi.de>
2014-07-31 08:57:55 +0200

    #2383 - [TMPL] Show Users Limited Cache Management

---
+++ Helper/Data.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: akn
+ * Date: 18.03.14
+ * Time: 15:37
+ */
+
+class IMI_EasyCacheRefresh_Helper_Data extends Mage_Core_Helper_Abstract {
+
+
+
+} 
---
+++ Model/Observer.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * iMi Magento Module
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject of iMi.
+ * You may not be allowed to change the sources
+ * without authorization of iMi digital GmbH.
+ *
+ * @copyright  Copyright (c) 2014 iMi digital GmbH (http://www.iMi.de)
+ * @author iMi digital GmbH <info@iMi.de>
+ * @license proprietary
+ * @category IMI
+ * @package IMI_ViledaLimitedCacheManagement
+ */
+
+class IMI_EasyCacheRefresh_Model_Observer {
+
+
+}
