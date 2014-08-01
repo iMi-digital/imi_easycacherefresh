dda06667e80e6202380d5c0a6643b16d89e1e73e
Alexander Menk <a.menk@imi.de>
2014-08-01 11:13:14 +0200

    Remove additional block, remove buttons/menu entries via events

--- src/app/code/community/IMI/EasyCacheRefresh/Helper/Data.php
+++ src/app/code/community/IMI/EasyCacheRefresh/Helper/Data.php
@@ -1,13 +1,32 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: akn
- * Date: 18.03.14
- * Time: 15:37
+ * iMi Magento Module
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject of iMi.
+ * You may not be allowed to change the sources
+ * without authorization of iMi digital GmbH.
+ *
+ * @copyright  Copyright (c) 2014 iMi digital GmbH (http://www.iMi.de)
+ * @author iMi digital GmbH <info@iMi.de>
+ * @license OSL
+ * @category IMI
+ * @package IMI_EasyCacheRefresh
  */
 
 class IMI_EasyCacheRefresh_Helper_Data extends Mage_Core_Helper_Abstract {
 
+    /**
+     * Do we only have the limited cache management?
+     *
+     * @return bool
+     */
+    public function isLimited()
+    {
+        return Mage::getSingleton('admin/session')->isAllowed('system/limited_cache') &&
+            !Mage::getSingleton('admin/session')->isAllowed('system/cache');
+    }
 
 
 } 
--- src/app/code/community/IMI/EasyCacheRefresh/Model/Observer.php
+++ src/app/code/community/IMI/EasyCacheRefresh/Model/Observer.php
@@ -10,12 +10,75 @@
  *
  * @copyright  Copyright (c) 2014 iMi digital GmbH (http://www.iMi.de)
  * @author iMi digital GmbH <info@iMi.de>
- * @license proprietary
+ * @license OSL
  * @category IMI
- * @package IMI_ViledaLimitedCacheManagement
+ * @package IMI_EasyCacheRefresh
  */
 
 class IMI_EasyCacheRefresh_Model_Observer {
 
+    /**
+     * Rewrite Permissions for the cache management menu entry if we have only limited cache control (menu entry to be shown)
+     */
+    public function adminhtmlBlockHtmlBefore(Varien_Event_Observer $observer)
+    {
+        if (!($observer->getBlock() instanceof Mage_Adminhtml_Block_Page_Menu)) {
+            return;
+        }
 
+        $config = Mage::getSingleton('admin/config')->getAdminhtmlConfig();
+        $menu = $config->getNode('menu');
+
+        if (!Mage::getSingleton('admin/session')->isAllowed('system/cache')) {
+            $menu->system->children->cache->addChild('resource', 'system/limited_cache');
+        }
+    }
+
+    public function coreBlockAbstractToHtmlBefore(Varien_Event_Observer $observer)
+    {
+        $block = $observer->getBlock();
+        if ($block instanceof Mage_Adminhtml_Block_Widget_Grid_Massaction) {
+            $this->_customizeGrid($block);
+        } elseif ($block instanceof Mage_Adminhtml_Block_Cache) {
+            $this->_customizeMainBlock($block);
+        } elseif ($block instanceof Mage_Adminhtml_Block_page) {
+            $fullAction = $block->getRequest()->getModuleName() . '_' . $block->getRequest()->getControllerName() . '_' . $block->getRequest()->getActionName();
+            if ($fullAction != 'admin_cache_index') {
+                return;
+            }
+            $block->getChild('content')->unsetChild('cache.additional');
+
+        }
+    }
+
+    /**
+     * Remove en-/disable caches
+     *
+     * @param Mage_Adminhtml_Block_Cache_Grid $block
+     */
+    protected function _customizeGrid(Mage_Adminhtml_Block_Widget_Grid_Massaction $block)
+    {
+        if (Mage::helper('imi_easycacherefresh')->isLimited()) {
+            $block->removeItem('enable');
+            $block->removeItem('disable');
+        }
+    }
+
+    /**
+     * Remove buttons if limited; add easy refresh button
+     *
+     * @param Mage_Adminhtml_Block_Cache $block
+     */
+    protected function _customizeMainBlock(Mage_Adminhtml_Block_Cache $block) {
+        if (Mage::helper('imi_easycacherefresh')->isLimited()) {
+            $block->removeButton('flush_magento');
+            $block->removeButton('flush_system');
+        }
+
+        $block->addButton('refresh_invalidated', array(
+            'label'     => Mage::helper('imi_easycacherefresh')->__('Refresh Invalidated'),
+            'onclick'   => 'setLocation(\'' . $block->getUrl('*/*/refreshInvalidated') .'\')',
+            'class'     => 'delete',
+        ));
+    }
 }
